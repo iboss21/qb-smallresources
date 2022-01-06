05b6811e2fd41add9546ccd53dc112205ef29176
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-01-06 16:14:24 -0500

    updated mapname.lua
    corrected script

--- client/mapname.lua
+++ client/mapname.lua
@@ -3,10 +3,6 @@
 -- github https://github.com/iboss21
 -- Discord iBoss#2984
 
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-  AddTextEntry('FE_THDR_GTAO', '~y~ The Lux | ~b~ Empire, ~s~ RP..')
+CreateThread(function()
+  AddTextEntry('FE_THDR_GTA0', 'Text You Want')
 end)
