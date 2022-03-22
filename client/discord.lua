b2396c2c6eadf7651cca74cdb4ed9d077ea9234b
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-03-22 17:44:56 -0600

    framework update

--- client/discord.lua
+++ client/discord.lua
@@ -6,21 +6,21 @@ CreateThread(function()
 	SetDiscordAppId()
 
         -- Here you will have to put the image name for the "large" icon.
-	SetDiscordRichPresenceAsset('logogoldenlionnycmyhouse_-_copy')
+	SetDiscordRichPresenceAsset('logo_name')
         
         -- (11-11-2018) New Natives:
 
         -- Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('The Lux Empire RP. #1 Georgian Server on Fivem. Fully Licensed')
+        SetDiscordRichPresenceAssetText('This is a lage icon with text')
        
         -- Here you will have to put the image name for the "small" icon.
-        SetDiscordRichPresenceAssetSmall('logogoldenlionnycmyhouse_-_copy')
+        SetDiscordRichPresenceAssetSmall('logo_name')
 
         -- Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('Join The Most Popular #1 Georgian Server on Fivem. The Lux Empire RP.')
+        SetDiscordRichPresenceAssetSmallText('This is a lsmall icon with text')
 
         QBCore.Functions.TriggerCallback('smallresources:server:GetCurrentPlayers', function(result)
-            SetRichPresence('Players: '..result..'/128')
+            SetRichPresence('Players: '..result..'/64')
         end)
 
         -- (26-02-2021) New Native:
@@ -30,8 +30,8 @@ CreateThread(function()
             First paramater is the button index (0 or 1), second is the title and 
             last is the url (this has to start with "fivem://connect/" or "https://") 
         ]]--
-        SetDiscordRichPresenceAction(0, "Join The Lux Empire!", "fivem://connect/r33ogx:30120")
-        SetDiscordRichPresenceAction(1, "Join The Discord!", "https://discord.gg/TheLuxEmpire")
+        SetDiscordRichPresenceAction(0, "First Button!", "fivem://connect/localhost:30120")
+        SetDiscordRichPresenceAction(1, "Second Button!", "fivem://connect/localhost:30120")
 
         -- It updates every minute just in case.
 	Wait(60000)
--- config.lua
+++ config.lua
@@ -3,9 +3,10 @@ Config.MaxWidth = 5.0
 Config.MaxHeight = 5.0
 Config.MaxLength = 5.0
 Config.DamageNeeded = 100.0
+Config.IdleCamera = true
 Config.EnableProne = true
-Config.JointEffectTime = 120
-Config.RemoveWeaponDrops = false
+Config.JointEffectTime = 60
+Config.RemoveWeaponDrops = true
 Config.RemoveWeaponDropsTimer = 25
 Config.DefaultPrice = 20 -- carwash
 Config.DirtLevel = 0.1 --carwash dirt level
@@ -18,8 +19,8 @@ ConsumeablesEat = {
 }
 
 ConsumeablesDrink = {
-    ["raine"] = math.random(35, 54),
-    ["cola"] = math.random(35, 54),
+    ["water_bottle"] = math.random(35, 54),
+    ["kurkakola"] = math.random(35, 54),
     ["coffee"] = math.random(40, 50),
 }
 
@@ -33,6 +34,10 @@ Config.BlacklistedScenarios = {
     ['TYPES'] = {
         "WORLD_VEHICLE_MILITARY_PLANES_SMALL",
         "WORLD_VEHICLE_MILITARY_PLANES_BIG",
+        "WORLD_VEHICLE_AMBULANCE",
+        "WORLD_VEHICLE_POLICE_NEXT_TO_CAR",
+        "WORLD_VEHICLE_POLICE_CAR",
+        "WORLD_VEHICLE_POLICE_BIKE",
     },
     ['GROUPS'] = {
         2017590552,
@@ -43,32 +48,60 @@ Config.BlacklistedScenarios = {
 }
 
 Config.BlacklistedVehs = {
-    [`SHAMAL`] = false,
-    [`LUXOR`] = false,
-    [`LUXOR2`] = false,
-    [`JET`] = false,
-    [`LAZER`] = false,
-    [`BUZZARD`] = false,
-    [`ANNIHILATOR`] = false,
-    [`SAVAGE`] = false,
-    [`TITAN`] = false,
-    [`RHINO`] = false,
-    [`FIRETRUK`] = false,
-    [`MULE`] = false,
-    [`MAVERICK`] = false,
-    [`BLIMP`] = false,
-    [`AIRTUG`] = false,
-    [`CAMPER`] = false,
-    [`seashark`] = true,
+    [`SHAMAL`] = true,
+    [`LUXOR`] = true,
+    [`LUXOR2`] = true,
+    [`JET`] = true,
+    [`LAZER`] = true,
+    [`BUZZARD`] = true,
+    [`BUZZARD2`] = true,
+    [`ANNIHILATOR`] = true,
+    [`SAVAGE`] = true,
+    [`TITAN`] = true,
+    [`RHINO`] = true,
+    [`FIRETRUK`] = true,
+    [`MULE`] = true,
+    [`MAVERICK`] = true,
+    [`BLIMP`] = true,
+    [`AIRTUG`] = true,
+    [`CAMPER`] = true,
+    [`HYDRA`] = true,
+    [`OPPRESSOR`] = true,
+    [`technical3`] = true,
+    [`insurgent3`] = true,
+    [`apc`] = true,
+    [`tampa3`] = true,
+    [`trailersmall2`] = true,
+    [`halftrack`] = true,
+    [`hunter`] = true,
+    [`vigilante`] = true,
+    [`akula`] = true,
+    [`barrage`] = true,
+    [`khanjali`] = true,
+    [`caracara`] = true,
+    [`blimp3`] = true,
+    [`menacer`] = true,
+    [`oppressor2`] = true,
+    [`scramjet`] = true,
+    [`strikeforce`] = true,
+    [`cerberus`] = true,
+    [`cerberus2`] = true,
+    [`cerberus3`] = true,
+    [`scarab`] = true,
+    [`scarab2`] = true,
+    [`scarab3`] = true,
+    [`rrocket`] = true,
+    [`ruiner2`] = true,
+    [`deluxo`] = true,
 }
 
 Config.BlacklistedPeds = {
-    [`s_m_y_ranger_01`] = false,
-    [`s_m_y_sheriff_01`] = false,
-    [`s_m_y_cop_01`] = false,
-    [`s_f_y_sheriff_01`] = false,
-    [`s_f_y_cop_01`] = false,
-    [`s_m_y_hwaycop_01`] = false,
+    [`s_m_y_ranger_01`] = true,
+    [`s_m_y_sheriff_01`] = true,
+    [`s_m_y_cop_01`] = true,
+    [`s_f_y_sheriff_01`] = true,
+    [`s_f_y_cop_01`] = true,
+    [`s_m_y_hwaycop_01`] = true,
 }
 
 Config.Teleports = {
@@ -76,12 +109,12 @@ Config.Teleports = {
     [1] = {
         [1] = {
             coords = vector4(3540.74, 3675.59, 20.99, 167.5),
-            ["AllowVehicle"] = true,
+            ["AllowVehicle"] = false,
             drawText = '[E] Take Elevator Up'
         },
         [2] = {
             coords = vector4(3540.74, 3675.59, 28.11, 172.5),
-            ["AllowVehicle"] = true,
+            ["AllowVehicle"] = false,
             drawText = '[E] Take Elevator Down'
         },
 
@@ -90,12 +123,12 @@ Config.Teleports = {
     [2] = {
         [1] = {
             coords = vector4(909.49, -1589.22, 30.51, 92.24),
-            ["AllowVehicle"] = true,
+            ["AllowVehicle"] = false,
             drawText = '[E] Enter Coke Processing'
         },
         [2] = {
             coords = vector4(1088.81, -3187.57, -38.99, 181.7),
-            ["AllowVehicle"] = true,
+            ["AllowVehicle"] = false,
             drawText = '[E] Leave'
         },
     },
